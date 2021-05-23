c19a8d7d65ec76e96eb571fec7889e4497cfadd8
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-05-23 11:55:08 +0300

    Routing protocols

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {NavbarComponent} from './components/navbar/navbar.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'repo', component: NavbarComponent},
+  { path: 'user', component: NavbarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
