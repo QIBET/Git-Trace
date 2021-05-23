cce6abd214389c2c84d1e1cc2ea02629f3f38cfd
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-05-23 08:02:15 +0300

    Adding bootstrap

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-  api_key: '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';
+  production: false,
+  api_key: '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'
 };
 
 /*
