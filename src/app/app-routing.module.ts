1533537c2e2b89d4bc731104774c12d43023ca7f
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-05-23 16:04:51 +0300

    routing home file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {NavbarComponent} from './components/navbar/navbar.component';
-import {FindrepoComponent} from './components/findrepo/findrepo.component';
+/* import {NavbarComponent} from './components/navbar/navbar.component';
+ */import {FindrepoComponent} from './components/findrepo/findrepo.component';
 import {FinduserComponent} from './components/finduser/finduser.component';
+import {MyprofileComponent} from './components/myprofile/myprofile.component';
 
 
 const routes: Routes = [
-  {path: 'home', component:NavbarComponent},
+  {path: 'profile', component:MyprofileComponent},
   { path: 'repo', component: FindrepoComponent},
   { path: 'user', component: FinduserComponent},
-   { path: '', redirectTo:"/home", pathMatch:"full"},
+  { path: '', redirectTo:"/profile", pathMatch:"full"},
 ];
 
 @NgModule({
